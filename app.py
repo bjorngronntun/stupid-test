fd9382dfcd3c41b342d47a6258d66310cd1c5cf0
bjorngronntun <bjorngronntun@gmail.com>
2018-02-05 00:34:58 +0100

    Add JSON graph

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
-import numpy as np
-
+from flask import Flask, jsonify
 app = Flask(__name__)
+import json
 
-x = np.linspace(0,1,100)
+with open('data/graph.json') as f:
+    graph = json.load(f)
 
 @app.route('/')
 def home():
-    return str(np.sin(x))
+    return jsonify(graph)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(port=5000, debug=True)
